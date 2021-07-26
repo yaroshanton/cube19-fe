156729308397270190ced67e842bdb1d6d4b63f8
luk2sh <s.luk2sh@gmail.com>
2021-07-26 14:48:22 +0300

    [004] - eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import Title from './components/Title'
-import HighestScorers from './components/HighestScorers'
-import LeadersTable from './components/LeadersTable'
+import Title from './components/Title';
+import HighestScorers from './components/HighestScorers';
+import LeadersTable from './components/LeadersTable';
 
 function App() {
-  return (
-    <>
-    <Title/>
-    <HighestScorers/>
-    <LeadersTable/>
-    </>
-  );
+	return (
+		<>
+			<Title />
+			<HighestScorers />
+			<LeadersTable />
+		</>
+	);
 }
 
 export default App;
