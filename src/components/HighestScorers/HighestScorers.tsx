99f89b08bc2cb339b30862389aead2fc57ec6dc9
yaroshanton <on.yaroshanton@gmail.com>
2021-07-27 12:25:42 +0300

    add reducer and getting leaders

--- src/components/HighestScorers/HighestScorers.tsx
+++ src/components/HighestScorers/HighestScorers.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useDispatch } from 'react-redux'
+import operations from "../../redux/leaders/leadersOperations"
 import './HighestScorers.scss'
 
 const HighestScorers = () => {
+
+  const dispatch: any = useDispatch()
+  useEffect(() => dispatch(operations.fetchLeaders()), []);
+
   return (
     <h1>HighestScorers</h1>
   )
