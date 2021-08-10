68ef067da042c8598d36d057467bce3fd0281684
yaroshanton <on.yaroshanton@gmail.com>
2021-08-10 11:31:04 +0300

    [TASK] - Fix logic add in aarrays array

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -14,28 +14,29 @@ const ListItem = (props: TListItemProps) => {
 
 	return (
 		<div className="wrapper-list-item">
-			{leaders.map((leader: ILeader) => {
-				return (
-					<li key={leader.id} className="list-item">
-						<div className="list-item__place">{leader.position}st</div>
-						<img src={UserImage} alt="user" className="list-item__image" />
-						<div className="list-item__score">{leader.score}</div>
-						<div className="list-item__name ">{leader.name}</div>
-						<div className="list-item__changes">
-							{leader.change === 0 ? 'No Change' : `${leader.change}point`}
-						</div>
-						<div
-							className="list-item__edit cursor-pointer"
-							onClick={() => updateOneLeader(leader)}
-							onKeyDown={() => updateOneLeader}
-							role="button"
-							tabIndex={0}
-						>
-							<img src={PencilImage} alt="pencil" />
-						</div>
-					</li>
-				);
-			})}
+			{leaders &&
+				leaders.map((leader: ILeader) => {
+					return (
+						<li key={leader.id} className="list-item">
+							<div className="list-item__place">{leader.position}st</div>
+							<img src={UserImage} alt="user" className="list-item__image" />
+							<div className="list-item__score">{leader.score}</div>
+							<div className="list-item__name ">{leader.name}</div>
+							<div className="list-item__changes">
+								{leader.change === 0 ? 'No Change' : `${leader.change}point`}
+							</div>
+							<div
+								className="list-item__edit cursor-pointer"
+								onClick={() => updateOneLeader(leader)}
+								onKeyDown={() => updateOneLeader}
+								role="button"
+								tabIndex={0}
+							>
+								<img src={PencilImage} alt="pencil" />
+							</div>
+						</li>
+					);
+				})}
 		</div>
 	);
 };
