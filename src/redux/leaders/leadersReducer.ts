b0ef3b3271f02f7f493bb0e730deb78eae52f2cd
yaroshanton <on.yaroshanton@gmail.com>
2021-08-10 17:28:38 +0300

    [TASK] - Add logic history in func. component

--- src/redux/leaders/leadersReducer.ts
+++ src/redux/leaders/leadersReducer.ts
@@ -1,7 +1,12 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { ILeader } from './interfaces/leder.types';
-import { fetchLeadersSuccess, editLeadersAction, addLeadersAction } from './actionTypes';
-import { createLeaderOperation, editLeaderOperation, getLeadersOperations } from './leadersOperations';
+import { fetchLeadersSuccess, editLeadersAction, addLeadersAction, addOldLeadersAction } from './actionTypes';
+import {
+	createLeaderOperation,
+	editLeaderOperation,
+	getLeadersOperations,
+	addOldLeadersOperation,
+} from './leadersOperations';
 
 const initState: ILeader[] = [];
 
@@ -9,4 +14,10 @@ export const leaders = createReducer(initState, {
 	[fetchLeadersSuccess.type]: getLeadersOperations,
 	[editLeadersAction.type]: editLeaderOperation,
 	[addLeadersAction.type]: createLeaderOperation,
+});
+
+const initStateOldLeaders: ILeader[][] = [];
+
+export const oldLeaders = createReducer(initStateOldLeaders, {
+	[addOldLeadersAction.type]: addOldLeadersOperation,
 });
